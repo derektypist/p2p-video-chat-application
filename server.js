ed612df4abd8c4baf002753d10099d868d9e08ea
Derek Dhammaloka <41344434+derektypist@users.noreply.github.com>
2021-09-07 11:10:20 +0000

    updated server.js to include starting server

--- server.js
+++ server.js
@@ -1,3 +1,16 @@
 // Set up constants
 const express = require("express");
 const app = express();
+
+app.use('/public',express.static(process.cwd() + '/public'));
+
+app.use(express.json());
+app.use(express.urlencoded({extended:true}));
+app.get('/', (req, res) => {
+  res.sendFile(process.cwd() + '/views/index.html');
+});
+
+// Start the server
+app.listen(3000, () => {
+  console.log('Server running on Port 3000');
+});
