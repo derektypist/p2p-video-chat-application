0dd7354b863dc102b4e3f6b4528218e704645aaa
Derek Dhammaloka <41344434+derektypist@users.noreply.github.com>
2021-09-08 16:41:09 +0000

    Added variables at index.js

---
+++ public/index.js
@@ -0,0 +1,9 @@
+// Set Up Variables
+
+const localVideo = document.querySelector('.local-stream');
+const remoteVideo = document.querySelector('.remote-stream');
+let room;
+let localStream;
+let remoteStream;
+
+
