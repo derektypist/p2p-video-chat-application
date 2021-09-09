c81c1f9e83f6a33b2fe9b958d4383cf5c5984dbc
Derek Dhammaloka <41344434+derektypist@users.noreply.github.com>
2021-09-09 11:51:26 +0000

    updated index.js

--- public/index.js
+++ public/index.js
@@ -1,4 +1,19 @@
+// Import Modules
+
+import {initializeApp} from 'firebase/app';
+import {getFirestore, collection, doc, setDoc, getDoc, getDocs, addDoc, updateDoc, deleteDocs, onSnapshot} from 'firebase/firestore';
+
 // Set Up Variables
+const app = initializeApp({projectId: "p2p-video-chat-application"});
+const db = getFirestore(app);
+const servers = {
+  iceServers: [
+    {
+      urls: ['stun:stun1.l.google.com:19302', 'stun:stun2.l.google.com:19302'],
+    },
+  ],
+  iceCandidatePoolSize: 10,
+};
 
 const localVideo = document.querySelector('.local-stream');
 const remoteVideo = document.querySelector('.remote-stream');
